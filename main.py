891420e38e62f8869e16031f5956da86be5a2d57
DuckMasterAl <43557963+DuckMasterAl@users.noreply.github.com>
2020-10-10 18:00:16 -0400

    Colors!

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ with open(quotefile) as json_file:
     data = json.load(json_file)
 quote = random.choice(data['quotes'])
 pyperclip.copy(quote)
-print(f'The following quote has been copied to your clipboard:\n{quote}')
+print(f'The following quote has been copied to your clipboard:\n\033[94m{quote}\033[0m')
